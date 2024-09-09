c2f733826099c3937f60cbe6a022388224ba7a56
Nico van den Berg <ravensclaw3@gmail.com>
2024-09-09 22:13:35 +0200

    feat: Create sting counter function

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("hello world")
+def strCount(input):
+    words = input.split()
+    return len(words)
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+
+    print(strCount(file_contents))
+
+
+if __name__ == "__main__":
+    main()
