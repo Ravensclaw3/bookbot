b912cf5d22754e0b0db759d633ea22f99211cb6b
Nico van den Berg <ravensclaw3@gmail.com>
2024-09-09 22:38:17 +0200

    feat: Create character count function.

--- main.py
+++ main.py
@@ -6,11 +6,23 @@ def strCount(input):
     words = input.split()
     return len(words)
 
+def charCount(input):
+    characters = dict()
+    lowered_input = input.lower()
+    for i in lowered_input:
+        if i in characters:
+            characters[i] += 1
+        else:
+            characters[i] = 1
+    return characters
+
 def main():
-    filelocation = "books/frankenstein.txt"
-    file_contents = getFileContent(filelocation)
+    file_location = "books/frankenstein.txt"
+    file_contents = getFileContent(file_location)
 
     print(strCount(file_contents))
+
+    print (charCount(file_contents))
 
 
 if __name__ == "__main__":
